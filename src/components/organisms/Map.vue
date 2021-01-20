a47fbfb932afa709fcef0c49e1e37b11aab9807a
Alan <ascandrett@esriaustralia.com.au>
2021-01-20 15:28:29 +1000

    back to square 1, but better

--- src/components/organisms/Map.vue
+++ src/components/organisms/Map.vue
@@ -2,6 +2,7 @@
   <div class="MapView">
     <LMap ref="LeafletMap" :zoom="zoom" :center="center">
       <LTileLayer :url="url"></LTileLayer>
+      <LFeatureGroup ref="SketchLayer" @ready="addControls()" />
       <MapButton
         ><StatisticsPanel
           :buildingsCount="buildingStatistics.buildingsCount"
@@ -12,30 +13,33 @@
 </template>
 
 <script>
-import L from "leaflet";
-import { LMap, LTileLayer } from "vue2-leaflet";
-import "@geoman-io/leaflet-geoman-free";
-import "@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css";
-import "leaflet-draw";
+import { LMap, LTileLayer, LFeatureGroup } from "vue2-leaflet";
 import query from "../../controllers/queryController";
 import MapButton from "../molecules/MapButton";
 import StatisticsPanel from "../atoms/StatisticsPanel";
+// Spatial Libs
+import L from "leaflet";
+import "leaflet-draw";
+import "leaflet-draw/dist/leaflet.draw.css";
+import * as turf from "@turf/turf";
 
 export default {
   name: "Map",
   components: {
     LMap,
     LTileLayer,
+    LFeatureGroup,
     MapButton,
     StatisticsPanel,
   },
   data() {
     return {
-      url: "https://cdn.lima-labs.com/{z}/{x}/{y}.png?free",
+      url: "https://cdn.lima-labs.com/{z}/{x}/{y}.png?free", // Romanization of local language labels
       zoom: 15,
-      center: [35.67319515681665, 139.74308967590332],
+      center: [35.67319515681665, 139.74308967590332], // Tokyo
       bounds: null,
-      buildingLayer: undefined,
+      buildingLayers: [],
+      queryPolygonLayers: [],
       buildingFootprintArea: undefined,
       queryPolygonArea: undefined,
     };
@@ -44,71 +48,81 @@ export default {
     map() {
       return this.$refs.LeafletMap.mapObject;
     },
+    sketchLayer() {
+      return this.$refs.SketchLayer;
+    },
     buildingStatistics() {
       let statistics = {
         buildingsCount: undefined,
         buildingsLandUse: undefined,
       };
-      if (this.buildingLayer && this.buildingFootprintArea && this.queryPolygonArea) {
+      if (this.buildingFootprintArea && this.queryPolygonArea) {
         // Total amount of structures
         statistics.buildingsCount = Object.keys(
-          this.buildingLayer._layers
+          this.buildingLayers[0]._layers
         ).length;
         // Landuse relative to structures
-        statistics.buildingsLandUse = this.buildingFootprintArea / this.queryPolygonArea * 100
+        statistics.buildingsLandUse =
+          (this.buildingFootprintArea / this.queryPolygonArea) * 100;
       }
-      console.log(this.buildingFootprintArea, this.queryPolygonArea)
+      //console.log(this.buildingFootprintArea, this.queryPolygonArea);
       return statistics;
     },
   },
   mounted() {
-    // add leaflet-geoman controls with some options to the map
-    this.addControls();
-    // Event listener to inject OSM geometry into map on draw complete.
-    this.map.on("pm:drawend", () => {
+    // Event listeners
+    // Inject OSM geometry into map on draw complete.
+    this.map.on("draw:created", (e) => {
+      // Standard layer accessors do not work, so we manage them internally.
+      // Thus we keep track of sketch layers ourselves, and remove them as needed.
+      const oldLayer = this.queryPolygonLayers.pop();
+      if (oldLayer) oldLayer.remove();
+      // Add the newest sketch layer to the map.
+      this.queryPolygonLayers.push(e.layer);
+      this.map.addLayer(e.layer);
       this.buildingFootprintArea = 0;
       this.queryPolygonArea = 0;
       this.getGeometry();
     });
   },
   methods: {
+    getArea(geoJson) {
+      const polygon = turf.polygon(geoJson); // m2 default units
+      return turf.area(polygon);
+    },
     addControls() {
-      // Geoman Draw Tools
-      this.map.pm.addControls({
-        position: "topleft",
-        drawPolyline: false,
-        drawCircle: false,
-        drawCircleMarker: false,
-        drawMarker: false,
-        cutPolygon: false,
-        removalMode: false,
-        editMode: false,
-        oneBlock: true,
+      // Leaflet Draw Tools
+      const drawControl = new L.Control.Draw({
+        draw: {
+          polyline: false,
+          circle: false,
+          circlemarker: false,
+          marker: false,
+        },
+        edit: {
+          featureGroup: this.$refs.SketchLayer.mapObject,
+          edit: false,
+          remove: false,
+        },
       });
+      this.map.addControl(drawControl);
     },
     getGeometry() {
-      const geoJson = this.map.pm.getGeomanDrawLayers(true).toGeoJSON();
-      // Area in Meters Squared
-      this.queryPolygonArea = L.GeometryUtil.geodesicArea(
-        this.map.pm.getGeomanDrawLayers(true).getLayers()[0].getLatLngs()[0]
-      );
-      // Remove existing geometry and previous drawn geometry
-      //if(this.buildingLayer) this.map.removeLayer(this.buildingLayer);
-      this.map.eachLayer(function (layer) {
-        if (!layer._url) layer.remove();
-      });
-      this.spatialQueryOSM(geoJson);
+      const geoJson = this.queryPolygonLayers[0].toGeoJSON();
+      this.queryPolygonArea = this.getArea(geoJson.geometry.coordinates);
+      this.spatialQueryOSM(geoJson.geometry.coordinates);
     },
     addGeoJSONtoMap(error, layer) {
-      this.buildingLayer = L.geoJson(layer, { pmIgnore: true });
-      this.buildingLayer.addTo(this.map);
+      if (this.buildingLayers.length) this.buildingLayers.pop().remove();
+      const buildingLayer = L.geoJson(layer, { pmIgnore: true });
+      this.buildingLayers.push(buildingLayer);
+      buildingLayer.addTo(this.map);
       this.calculateBuildingFootprintArea();
     },
-    spatialQueryOSM(geoJson) {
+    spatialQueryOSM(geometryCoordinates) {
       let latlon = "";
-      geoJson.features[0].geometry.coordinates[0].pop();
-      geoJson.features[0].geometry.coordinates[0].forEach((coordPair) => {
-        latlon += " " + coordPair[1] + " " + coordPair[0];
+      geometryCoordinates[0].forEach((coordinatePair) => {
+        latlon += " " + coordinatePair[1] + " " + coordinatePair[0];
       });
       // query, callback, options
       query.osm(
@@ -122,12 +136,10 @@ export default {
     },
     async calculateBuildingFootprintArea() {
       var totalArea = 0;
-      console.log(this.buildingLayer);
-      const layerList = this.buildingLayer.getLayers();
-      layerList.forEach(layer => {
-        let area = L.GeometryUtil.geodesicArea(layer.getLatLngs()[0]);
-        console.log(area)
-        totalArea += area
+      const layerList = this.buildingLayers[0].getLayers();
+      layerList.forEach((layer) => {
+        let area = this.getArea(layer.feature.geometry.coordinates);
+        totalArea += area;
       });
       this.buildingFootprintArea = parseFloat(totalArea.toFixed(2));
     },
